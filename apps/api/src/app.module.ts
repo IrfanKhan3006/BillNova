04e9a2cdb0bf37879122afd700763c89bdea1591
Irfan Khan <irfan.khan@devkraft.in>
2026-06-27 20:10:30 +0530

    update the error

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
@@ -8,7 +6,5 @@ import { TenantsModule } from './modules/tenants/tenants.module';
 
 @Module({
   imports: [HealthModule, AuthModule, UsersModule, TenantsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
